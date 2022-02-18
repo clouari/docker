8df3e8ccfdb1a777b9b0a6e7482845074188b8b6
Da hee <ekdudekgml35@naver.com>
2022-02-18 17:05:59 +0900

    include "conn.php"; 로 간단하게 코드 압축함

--- html/database/insert.php
+++ html/database/insert.php
@@ -1,13 +1,5 @@
 <?php
-$servername = "host.docker.internal";
-$username = "Jung";
-$password = "dahui";
-$mysql_database = 'myDB';
-$mysql_port = '52000';
-$mysql_charset = 'UTF8';
-
-// Create connection
-$conn = new mysqli($servername, $username, $password,$mysql_database, $mysql_port, $mysql_charset);
+include "conn.php";
 
 $sql = "
 INSERT INTO MyGuests (firstname, lastname, email)
