39bcccb6cdb20872b70790549f8be53e3b929183
Da hee <ekdudekgml35@naver.com>
2022-02-18 16:31:52 +0900

    로그인화면 만들기

---
+++ html/practice/login.php
@@ -0,0 +1,11 @@
+<form method="post" action="ok.php">
+    <center>
+            ID:
+            <input type="text" name="id" placeholder="ID를 입력 해 주세요">
+            <br/>
+            PW:
+            <input type="text" name="pw" placeholder="패스워드를 입력 해 주세요">
+            <br/>
+            <input type="submit" value="로그인">
+    </center>
+</form>
