f6167eb8ac8911542aa4adff0a6296999c89d351
Da hee <ekdudekgml35@naver.com>
2022-02-18 16:48:04 +0900

    바둑판 만들기

---
+++ html/make/mop_input.php
@@ -0,0 +1,14 @@
+
+<center>
+<h1>바둑판만들기</h1>
+
+<form method="get" action="rel.php">
+
+
+        가로 <input type="number" name="col">개 <br />
+        세로 <input type="number" name="row">개
+            <br/>
+            <input type="submit" value="만들기">
+</center>
+
+</form>
